4be9d761c150abc4228987134bf7f2404b97e6ec
dabu76 <153888541+dabu76@users.noreply.github.com>
2025-09-11 20:13:56 +0900

    Add files via upload

---
+++ Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :insData="insData" />
+    <Post :insData="insData" />
+    <Post :insData="insData" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  name: "ContainerPage",
+  props: {
+    insData: Object,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style></style>
